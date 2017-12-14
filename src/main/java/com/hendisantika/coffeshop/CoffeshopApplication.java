1702263b1f63449db300c3b9b33c47f41c18f5ee
Hendi Santika <hendisantika@gmail.com>
2017-12-15 06:13:29 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/coffeshop/CoffeshopApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.coffeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeshopApplication.class, args);
+    }
+}
